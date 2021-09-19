ec61b392cd7ac11c6e36e00bddb45f2ffd2195aa
hunman89 <hunman89@gmail.com>
2021-09-19 22:15:42 +0900

    add. footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import Footer from "./component/Footer";
 import Header from "./component/Header";
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header/>
+        <Header />
       </header>
+      <div style={{ height: 500 }}></div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }

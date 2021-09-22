08578a8a2f34cf9506701090cd643785a925e1a6
hunman89 <hunman89@gmail.com>
2021-09-22 15:59:11 +0900

    update. footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,6 +1,7 @@
 import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import ShortLogo from "../images/short-logo.png";
+import MailRoundedIcon from "@mui/icons-material/MailRounded";
 
 const text = {
   color: "white",
@@ -9,10 +10,10 @@ const text = {
 
 export default function Footer() {
   return (
-    <Grid style={{ height: 400, backgroundColor: "green" }}>
+    <Grid sx={{ height: 250, backgroundColor: "#5c6bc0" }}>
       <Container sx={{ padding: 6 }}>
         <Grid xs={12} container justifyContent="center">
-          <Grid xs={3} sx={{ gap: 2 }} container item justifyContent="center">
+          <Grid xs={3} sx={{ gap: 1 }} container item justifyContent="center">
             <img src={ShortLogo} height="20" alt="long-zeta-food-lab-logo" />
             <Typography sx={text}>주식회사 제타푸드랩</Typography>
           </Grid>
@@ -27,12 +28,12 @@ export default function Footer() {
           </Grid>
         </Grid>
         <Grid xs={12} sx={{ paddingTop: 2 }} container justifyContent="center">
-          <Grid xs={3} container item justifyContent="center">
+          <Grid xs={3} container item justifyContent="left">
             <Typography sx={text}>
               © 2021. ZETA FOOD LAB Inc. All Rights Reserved.
             </Typography>
           </Grid>
-          <Grid xs={3} container item justifyContent="center">
+          <Grid xs={3} container item justifyContent="left">
             <Typography sx={text}>광교연구소</Typography>
             <Typography sx={text}>
               경기도 수원시 영통구 창룡대로 256번길 77, 에이스광교타워3차 404호
@@ -42,7 +43,7 @@ export default function Footer() {
             <Typography sx={text}>대표번호 1899-4258</Typography>
           </Grid>
           <Grid xs={3} container item justifyContent="center">
-            <Typography sx={text}></Typography>
+            <MailRoundedIcon />
           </Grid>
         </Grid>
       </Container>

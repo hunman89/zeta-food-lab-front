64b2cb17f48b8eda9a3dd3ddeb9bdcc2da5b6eba
hunman89 <hunman89@gmail.com>
2022-04-14 21:53:20 +0900

    add. 기업개요 페이지

--- pages/company/overview.tsx
+++ pages/company/overview.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { NextPage } from "next";
 import Layout from "../../components/layout";
 import Head from "next/head";
+import Logo from "../../public/logo.png";
+import Image from "next/image";
 
 const Overview: NextPage = () => {
   return (
@@ -9,45 +11,37 @@ const Overview: NextPage = () => {
       <Head>
         <title>overview</title>
       </Head>
-      <div className="flex py-64 bg-slate-500 justify-center">
-        <div className="text-white font-bold text-5xl">기업소개</div>
-      </div>
-      <div className="w-full max-w-6xl mx-auto py-20">
-        <div className="flex justify-around text-2xl text-gray-500">
-          <div className="w-1/3 border-black border-b-2 pb-5 text-center">
-            CEO/CTO
-          </div>
-          <div className="w-1/3 border-gray-300 border-b-2 pb-5 text-center">
-            비전
-          </div>
-          <div className="w-1/3 border-gray-300 border-b-2 pb-5 text-center">
-            회사연혁
-          </div>
+      <div className="pt-24">
+        <div className="flex py-64 bg-slate-500 justify-center">
+          <div className="text-white font-bold text-5xl">기업 개요</div>
         </div>
-        <div className="flex flex-row justify-around pt-16">
-          <div className="flex flex-col">
-            <div className=" h-72 bg-slate-500 aspect-square rounded-xl"></div>
-            <p className="text-3xl font-medium text-center pt-5">CEO 지요셉</p>
-            <div className="flex mx-auto border-2 border-blue-700 text-blue-600 rounded-full mt-3 py-2 px-4 text-xl transition-color hover:bg-blue-600 hover:text-white">
-              연혁보기
+      </div>
+      <div className="flex flex-row w-full max-w-6xl mx-auto py-20 justify-around">
+        <div className="flex pt-32 justify-center">
+          <p className=" text-gray-700 font-medium text-2xl">
+            식품산업에 혁신적인 SOLUTION(&zeta;, zeta)을 제공하는 기업
+          </p>
+        </div>
+        <div className="  aspect-square">
+          <div>
+            <div className="flex justify-center">
+              <div className="flex bg-[#0053a6] aspect-square h-64 rounded-full justify-center">
+                <div className="text-white text-2xl my-auto">식품안전</div>
+              </div>
             </div>
-            <div className="flex flex-row justify-between text-center pt-5">
-              <div className="flex flex-col ">
-                <p className="text-4xl pb-2">33</p>
-                <p>과학논문</p>
+            <div className="flex flex-row -mt-20 -space-x-10">
+              <div className="flex bg-[#003873] aspect-square h-64 rounded-full justify-center">
+                <div className="text-white my-auto text-2xl">식품화학</div>
               </div>
-              <div>
-                <p className="text-4xl pb-2">8</p>
-                <p>수상경력</p>
-              </div>
-              <div>
-                <p className="text-4xl pb-2">18</p>
-                <p>연구경력</p>
+              <div className="flex bg-[#5186bd] aspect-square h-64 rounded-full justify-center">
+                <div className="text-white my-auto text-2xl">식품공학</div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col">
-            <div className=" h-72 bg-slate-500 aspect-square rounded-xl"></div>
+          <div className="flex pt-14  justify-center">
+            <div className="w-96">
+              <Image src={Logo} alt="Zeta Food Lab Logo"></Image>
+            </div>
           </div>
         </div>
       </div>

eda6c22aa21bae44a7cc6dc379bd8e65ab8c545f
hunman89 <hunman89@gmail.com>
2022-05-16 20:02:17 +0900

    add. funded project page

--- pages/research/national-project.tsx
+++ pages/research/national-project.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import { NextPage } from "next";
 import Layout from "../../components/layout";
 import Head from "next/head";
-import Image from "next/image";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const NationalProject: NextPage = () => {
+  const { t } = useTranslation("research");
   return (
     <Layout footer>
       <Head>
@@ -30,21 +31,27 @@ const NationalProject: NextPage = () => {
         </div>
       </div>
       <div className="w-full max-w-6xl mx-auto py-20 ">
-        <div className="flex text-3xl font-bold text-gray-800">2021</div>
-        <div className="flex flex-col text-xl text-gray-800 py-8 gap-5 pl-5">
-          <div>▷ 예비 창업 패키지</div>
-          <div>▷ 실전 창업 교육</div>
-          <div>▷ 메이커스페이스 시제품 제작 지원</div>
-          <div>▷ 농식품 벤처 육성 지원사업</div>
-          <div>▷ 과학기술분야 R&D 대체인력 활용 지원사업</div>
-        </div>
-        <div className="flex text-3xl font-bold text-gray-800">2022</div>
-        <div className="flex flex-col text-xl text-gray-800 py-8 gap-5 pl-5">
-          <div>▷ 농식품 벤처 육성 지원사업</div>
-          <div>▷ 초기 창업 패키지</div>
-          <div>▷ 시흥창업센터 시제품 제작 지원사업</div>
-          <div>▷ 특허전략 컨설팅 지원사업</div>
-          <div>▷ 공공시장 진출 프로그램 ( 지원완료, 선정 대기중 )</div>
+        <p className="indent-4 pb-20 text-2xl font-medium">
+          {t("FUNDED_COMMENT")}
+        </p>
+        <div>
+          <div className="flex text-3xl font-bold text-gray-800">2021</div>
+          <div className="flex flex-col text-xl text-gray-800 py-8 gap-3 pl-5">
+            <div>{t("FUNDED_2021_01")}</div>
+            <div>{t("FUNDED_2021_02")}</div>
+            <div>{t("FUNDED_2021_03")}</div>
+            <div>{t("FUNDED_2021_04")}</div>
+          </div>
+          <div className="flex text-3xl font-bold text-gray-800">2022</div>
+          <div className="flex flex-col text-xl text-gray-800 py-8 gap-3 pl-5">
+            <div>{t("FUNDED_2022_01")}</div>
+            <div>{t("FUNDED_2022_02")}</div>
+            <div>{t("FUNDED_2022_03")}</div>
+            <div>{t("FUNDED_2022_04")}</div>
+            <div>{t("FUNDED_2022_05")}</div>
+            <div>{t("FUNDED_2022_06")}</div>
+            <div>{t("FUNDED_2022_07")}</div>
+          </div>
         </div>
       </div>
     </Layout>
@@ -53,7 +60,7 @@ const NationalProject: NextPage = () => {
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
   props: {
-    ...(await serverSideTranslations(locale, ["common"])),
+    ...(await serverSideTranslations(locale, ["common", "research"])),
   },
 });
 
